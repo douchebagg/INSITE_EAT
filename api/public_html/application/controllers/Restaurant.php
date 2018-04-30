2f8bce88b2b23535b39c369de1bec9b3ee46e9e2
suphakiat <suphakiat08@gmail.com>
2018-04-30 12:00:48 +0000

    init function api

--- api/public_html/application/controllers/Restaurant.php
+++ api/public_html/application/controllers/Restaurant.php
@@ -11,15 +11,42 @@ class Restaurant extends REST_Controller {
 	}
 
     public function index_get() {
-
+    	$result = $this->Restaurant_Model->list_all_restaurants();
+    	if(!empty($result)) {
+			$this->response(array('restaurant' => $result), 200);
+    	} else {
+    		$this->response(array('restaurant' => array('No data in Restaurant api.')), 204);
+    	}
     }
 
     public function find_get($id) {
-
+    	$result = $this->Restaurant_Model->list_restaurant($id);
+    	if(!empty($result)) {
+			$this->response(array('restaurant' => $result), 200);
+    	} else {
+    		$this->response(array('restaurant' => 'No data in Restaurant api.'), 204);
+    	}
     }
 
     public function index_post() {
+    	$data = array(
+    		'RES_ID' => $this->post('id'),
+    		'RES_NAME' => $this->post('name'),
+    		'RES_REVIEW' => $this->post('review'),
+    		'RES_LOCALTION' => $this->post('location'),
+    		'OPENNING_TIME' => $this->post('openning'),
+    		'CLOSING_TIME' => $this->post('closing'),
+    		'RES_PHONE' => $this->post('phone'),
+    		'RES_SCORE' => $this->post('score')
+    	);
+		$result = $this->Restaurant_Model->add_restaurant($data);
         
+		if($result) {
+            $result = $this->Restaurant_Model->add_restaurant($data['RES_ID']);
+			$this->response(array('restaurant' => $result), 201);
+		} else {
+			$this->response(array('restaurant' => 'Add Restaurant failed.'), 200);
+		}
     }
 
     public function index_put($id) {
--- api/public_html/application/models/Restaurant_Model.php
+++ api/public_html/application/models/Restaurant_Model.php
@@ -4,15 +4,24 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Restaurant_Model extends CI_Model {
 
 	public function list_all_restaurants() {
-		
+		$sql = 'SELECT * FROM Restaurant ORDER BY RES_ID';
+		$result = $this->db->query($sql);
+		return json_encode($result->result_array());
 	}
 
 	public function list_restaurant($id) {
-
+		$sql = 'SELECT * FROM Restaurant WHERE RES_ID=? LIMIT 1';
+		$result = $this->db->query($sql, array('RES_ID' => $id));
+		return json_encode($result->result_array());
 	}
 
 	public function add_restaurant($data) {
-
+		$sql = 'INSERT INTO Restaurant VALUES(?, ?, ?, ?, ?, ?, ? ,?)';
+		$this->db->trans_begin();
+		$this->db->query($sql, $data);
+		$result = $this->db->trans_status();
+		$this->db->trans_commit();
+		return $result;
 	}
 }
 ?>
