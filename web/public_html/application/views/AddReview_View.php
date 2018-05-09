0a8c0a2a43433dd228883450386fcff903e6e5d8
Chayan Suratruangchai <chayan_bang@hotmail.com>
2018-05-09 07:57:52 +0000

    Map Marker

--- web/public_html/application/views/AddReview_View.php
+++ web/public_html/application/views/AddReview_View.php
@@ -30,37 +30,113 @@
                     $scope.restaurant = response.data.restaurant;
                     $scope.fd = {};
                     $scope.fd.RES_ID = $scope.restaurant[0].RES_ID;
+                    $scope.fd.FOOD_NAME = null;
+                    $scope.fd.FOOD_REVIEW = null;
+                    $scope.fd.FOOD_PRICE = null;
+                    $scope.fd.FOOD_SCORE = '0';
+                    $scope.fd.FOOD_IMAGE = null;
                 });
 
             $scope.add_res = () => {
                 $scope.res.POST_BY = "<?= $this->session->userdata('user_data')['token'] ?>";
+                $scope.res.OPENNING_TIME = document.getElementById('openning').value;
+                $scope.res.CLOSING_TIME = document.getElementById('closing').value;
+                $scope.res.RES_IMAGE = image_value();
                 $http.post('https://ec2-13-250-12-231.ap-southeast-1.compute.amazonaws.com/~api/restaurant', $scope.res);
             }
 
             $scope.add_food = () => {
                 $scope.fd.POST_BY = "<?= $this->session->userdata('user_data')['token'] ?>";
+                $scope.fd.RES_IMAGE = image_value();
                 res_id = $scope.fd.RES_ID;
                 $http.post('https://ec2-13-250-12-231.ap-southeast-1.compute.amazonaws.com/~api/restaurant/' + res_id + '/food', $scope.fd);
             }
         });
 
-        function file_upload() {
-            document.getElementById('photo').click();
-        }
-        function display_upload() {
-            document.getElementById('filedisplay').innerHTML = document.getElementById('photo').value.substring(12);
-            document.getElementById('filedisplay_2').innerHTML = document.getElementById('photo').value.substring(12);
+        function image_value() {
+            if(document.getElementById('photo_res').value) {
+                return document.getElementById('photo_res').value.substring(12);
+            } else if(document.getElementById('photo_food').value) {
+                return document.getElementById('photo_food').value.substring(12);
+            } else {
+                return null;
+            }
         }
 
-        function initMap(latitude, longitude) {
-            var latlng = new google.maps.LatLng(13.2724, 100.9234);
-            map = new google.maps.Map(document.getElementById('map_canvas'), {
-                center: latlng,
-                zoom: 18
-            });
+        function file_upload(type) {
+            if(type == 0) {
+                document.getElementById('photo_res').click();
+            } else {
+                document.getElementById('photo_food').click();
+            }
         }
+        function display_upload(type) {
+            if(type == 0) {
+                document.getElementById('filedisplay').innerHTML = document.getElementById('photo_res').value.substring(12);
+            } else {
+                document.getElementById('filedisplay_2').innerHTML = document.getElementById('photo_food').value.substring(12);
+            }
+        }
+
+      // In the following example, markers appear when the user clicks on the map.
+      // The markers are stored in an array.
+      // The user can then click an option to hide, show or delete the markers.
+      var map;
+      var markers = [];
+
+      function initMap() {
+        var haightAshbury = {lat: 13.2860, lng: 100.9254};
+
+        map = new google.maps.Map(document.getElementById('map_canvas'), {
+          zoom: 16,
+          center: haightAshbury,
+        });
+
+        // This event listener will call addMarker() when the map is clicked.
+        map.addListener('click', function(event) {
+          addMarker(event.latLng);
+        });
+
+        google.maps.event.addListener(map,'click',function(event) {                
+                document.getElementById('latclicked').innerHTML = event.latLng.lat();
+                document.getElementById('longclicked').innerHTML =  event.latLng.lng();
+        });
+
+        marker.addListener('click', function(event) {              
+              document.getElementById('latclicked').innerHTML = event.latLng.lat();
+              document.getElementById('longclicked').innerHTML =  event.latLng.lng();
+        });
+      }
+
+      // Adds a marker to the map and push to the array.
+      function addMarker(location) {
+        var marker = new google.maps.Marker({
+          position: location,
+          map: map
+        });
+        clearMarkers();
+        markers = [];
+        markers.push(marker);
+      }
+      
+      // Sets the map on all markers in the array.
+      function setMapOnAll(map) {
+        for (var i = 0; i < markers.length; i++) {
+          markers[i].setMap(map);
+        }
+      }
+
+      // Removes the markers from the map, but keeps them in the array.
+      function clearMarkers() {
+        setMapOnAll(null);
+      }
+
+    </script>
+    <script async defer
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBnckJfCM0a0gmuUUpvzrmUHZgqavIZIkk&callback=initMap">
     </script>
     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBnckJfCM0a0gmuUUpvzrmUHZgqavIZIkk&libraries=places&callback=initMap" async defer></script>
+    
 </head>
 <body ng-controller="myctrl">
     <div class="container" style="padding-bottom: 100px;">
@@ -107,7 +183,7 @@
 
                 <div class="row" style="background: #f9f9f9; box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24); font-size: 18px; padding: 20px 0px;">
                     <div class="form-group" ng-show="type === 'Restaurant'">
-                        <form action="<?= base_url('review/add') ?>" method="post" ng-submit="add_res()">
+                        <form action="<?= base_url('review/add') ?>" method="post" ng-submit="add_res()" enctype="multipart/form-data">
                             <div class="row col-md-12" style="margin-left: 0px">
                                 <div class="form-group col-md-12" ng-init="res.RES_NAME = null">
                                     <label style="font-size: 14px">Restaurant Name</label>
@@ -119,11 +195,11 @@
                                 </div>
                                 <div class="form-group col-md-6" ng-init="res.OPENNING_TIME = null">
                                     <label style="font-size: 14px">Openning Time</label>
-                                    <input type="text" class="form-control" data-field="time" ng-model="res.OPENNING_TIME" required readonly placeholder="Openning Time" style="background: #fff; cursor: unset;">
+                                    <input type="text" class="form-control" data-field="time" id="openning" required readonly placeholder="Openning Time" style="background: #fff; cursor: unset;">
                                 </div>
                                 <div class="form-group col-md-6" ng-init="res.CLOSING_TIME = null">
                                     <label style="font-size: 14px">Closing Time</label>
-                                    <input type="text" class="form-control" data-field="time" ng-model="res.CLOSING_TIME" required readonly placeholder="Closing Time" style="background: #fff; cursor: unset;">
+                                    <input type="text" class="form-control" data-field="time" id="closing" required readonly placeholder="Closing Time" style="background: #fff; cursor: unset;">
                                 </div>
                                 <div class="form-group col-md-12" ng-init="res.RES_PHONE = null">
                                     <label style="font-size: 14px">Phone</label>
@@ -142,14 +218,13 @@
                                 </div>
                                 <div class="form-group col-md-12" ng-init="res.RES_LATITUDE = 13.2724; res.RES_LONGITUDE = 100.9234">
                                     <div id="map_canvas" class="col-md-12" style="margin-top: 10px; width: 100%; height: 250px"></div>
+                                    <div id="latclicked"></div>
+                                    <div id="longclicked"></div>
                                 </div>
                                 <div class="form-group col-md-12" style="margin-top: 10px;" ng-init="res.RES_IMAGE = null">
-                                    <div id="filechoose" class="btn btn-default" onclick='file_upload()'>Choose File</div>
-                                    <small id="filedisplay" class="text-muted">
-                                        <span ng-show="x.RES_IMAGE == null">Choose images for restaurant.</span>
-                                        <span ng-show="x.RES_IMAGE != null">{{x.RES_IMAGE}}</span>
-                                    </small>
-                                    <input type="file" id="photo" name="photo" onchange="display_upload()" accept=".png, .jpg, .jpeg" style="display: none;">
+                                    <div id="filechoose" class="btn btn-default" onclick='file_upload(0)'>Choose File</div>
+                                    <small id="filedisplay" class="text-muted">Choose images for restaurant.</small>
+                                    <input type="file" id="photo_res" name="photo" onchange="display_upload(0)" accept=".png, .jpg, .jpeg" style="display: none;">
                                 </div>
                             </div>
                             <div class="row col-md-12 text-right">
@@ -159,7 +234,7 @@
                     </div>
 
                     <div class="form-group" ng-show="type === 'Food'">
-                        <form action="<?= base_url('review/add') ?>" method="post" ng-click="add_food()">
+                        <form action="<?= base_url('review/add') ?>" method="post" ng-submit="add_food()" enctype="multipart/form-data">
                             <div class="row col-md-12" style="margin-left: 0px">
                                 <div class="form-group col-md-12">
                                     <label style="font-size: 14px">Restaurant</label>
@@ -177,19 +252,23 @@
                                 </div>
                                 <div class="form-group col-md-6">
                                     <label style="font-size: 14px">Price</label>
-                                    <input type="text" class="form-control" ng-model="fd.FOOD_PRICE" placeholder="Price" min="1" required>
+                                    <input type="text" class="form-control" ng-model="fd.FOOD_PRICE" placeholder="Price" required pattern="[0-9]+">
                                 </div>
                                 <div class="form-group col-md-6">
                                     <label style="font-size: 14px">Score</label>
-                                    <input type="text" class="form-control" ng-model="fd.FOOD_SCORE" placeholder="Score" min="0" max="5" required>
+                                    <select class="form-control" ng-model="fd.FOOD_SCORE">
+                                        <option value="0">0</option>
+                                        <option value="1">1</option>
+                                        <option value="2">2</option>
+                                        <option value="3">3</option>
+                                        <option value="4">4</option>
+                                        <option value="5">5</option>
+                                    </select>
                                 </div>
                                 <div class="form-group col-md-12" style="margin-top: 10px;">
-                                    <div id="filechoose" class="btn btn-default" onclick='file_upload()'>Choose File</div>
-                                    <small id="filedisplay_2" class="text-muted">
-                                        <span ng-show="x.RES_IMAGE == NULL">Choose images for food.</span>
-                                        <span ng-show="x.RES_IMAGE != NULL">{{x.FOOD_IMAGE}}</span>
-                                    </small>
-                                    <input type="file" id="photo" name="photo" onchange="display_upload()" accept=".png, .jpg, .jpeg" style="display: none;">
+                                    <div id="filechoose" class="btn btn-default" onclick='file_upload(1)'>Choose File</div>
+                                    <small id="filedisplay_2" class="text-muted">Choose images for food.</small>
+                                    <input type="file" id="photo_food" name="photo" accept=".png, .jpg, .jpeg" style="display: none;">
                                 </div>
                             </div>
                             <div class="row col-md-12 text-right">
@@ -199,7 +278,17 @@
                     </div>
                 </div>
             </div>
-        </div>{{fd}}
+        </div>
+
+        <?php if(isset($added) && $added == true) { ?>
+        <div class="alert alert-success fade in">
+            Add Review <strong>successfully</strong>, return to <a href="<?= base_url('review') ?>" class="alert-link">My Review</a>.
+        </div>
+        <?php } else if(isset($exception)) { ?>
+        <div class="alert alert-danger fade in">
+            <?= $exception ?>
+        </div>
+        <?php } ?>
 
         <a id="top" class="footer" href="">
             <i class="fa fa-level-up"></i>
